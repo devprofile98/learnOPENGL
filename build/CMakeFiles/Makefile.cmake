f9e507ec8676da45b12cb99d683cf5f826c0f4c2
Ahmad Mansoori <ahmadmansooripv@gmail.com>
2020-12-04 22:31:00 +0330

    add phong lighting model

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -51,5 +51,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/gl5.dir/DependInfo.cmake"
+  "CMakeFiles/gl6.dir/DependInfo.cmake"
   )
